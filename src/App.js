dce7375c56ac52b8d5837b1a219dcfce4850c5d9
jamesbruice <james.bruice@web.de>
2022-08-29 15:45:17 +0000

    test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React - coolify
+          Learn React - coolify 1
         </a>
       </header>
     </div>
